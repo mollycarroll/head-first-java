e07f1c8dd20a1870c610444b5a6a7a77b28957ee
mollycarroll <mollyajcarroll@gmail.com>
2022-11-27 17:55:47 -0700

    initial commit

---
+++ src/DrumKit.java
@@ -0,0 +1,10 @@
+class DrumKit {
+    boolean topHat = true;
+    boolean snare = true;
+    void playTopHat () {
+        System.out.println("ding ding da-ding");
+    }
+    void playSnare () {
+        System.out.println("bang bang ba-bang");
+    }
+}
---
+++ src/DrumKitTestDrive.java
@@ -0,0 +1,11 @@
+class DrumKitTestDrive {
+    public static void main(String [] args) {
+        DrumKit d = new DrumKit();
+        d.playSnare();
+        d.snare = false;
+        if (d.snare) {
+            d.playSnare();
+        }
+        d.playTopHat();
+    }
+}
